b2274e00da1d1521e1ea18f1b97e590d9c2536b8
Twinkle-thomas <twinklethomas52@gmail.com>
2025-10-28 18:00:35 +1300

    1. Create send message

--- message/views.py
+++ message/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from message.models import Message
+from message.serializers import MessageSerializer
+
 
 # Create your views here.
+@api_view(['POST'])
+def send_message(request):
+    sender = request.data.get('sender')
+    receiver = request.data.get('receiver')
+    content = request.data.get('content')
+    chat_room = request.data.get('chat_room')
+    message_serializer = MessageSerializer(data=request.data)
+    if message_serializer.is_valid():
+        message_serializer.save()
+        return Response({'message': 'Message sent successfully'},
+                        status=status.HTTP_201_CREATED)
+
